15903e68e6a454c747543e16c2132777a5dd76b1
Fiorella Effio Saldivar <Aweba@localhost.localdomain>
2017-11-19 13:19:47 -0500

    FIOREEEE

--- part-1/example-02/hello_echo_1.c
+++ part-1/example-02/hello_echo_1.c
@@ -9,9 +9,18 @@ int main (int argc, char *argv[]) {
 			
 		argv : contiene los valores de los argumentos ingresados por linea de comandos (y desde cualquier otro lado)
 
+			los argumentos ingresados son separados por espacios siempre y cuando estos no esten entre comillas o no
+			sean escapados ("\<espacios>")
+			 los argumentos son ordenados en una lista de la siguiente forma
+			arg[0] : nombre del programa
+			arg[1] : argumento-1
+			.
+			.
+			.
+
 		notas : las expresiones como "%d", "%s" son especificadores de formato
 			(format specifiers)		
-	
+			las expresiones como "\t" o "\n" se conocen comos ecuencias de escape
 	
 	*/
 	for(int i = 1; i<argc; i++){
